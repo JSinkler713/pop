96fbd2b077f38f2bded0ab0f0971cf117561827f
James Sinkler <jamessinkler713@gmail.com>
2020-04-04 16:17:49 -0700

    check eventlistener directly on a bubble

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,10 @@
 'use strict'
 
 console.log('checking js file hooked up')
+//check event listener on bubble
+let bub= document.querySelector('.bubble')
+bub.addEventListener('click', ()=> {
+  console.log('clicked the ball')
+  bub.style.display = 'none';
+});
+
