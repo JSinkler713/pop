fdc01dfde22ac6b5334d79ec2ddbf76791003fe7
James Sinkler <jamessinkler713@gmail.com>
2020-04-04 20:37:14 -0700

    adds multiple bubble colors in random generator creator

--- scripts/app.js
+++ scripts/app.js
@@ -8,7 +8,12 @@ let balloonPop = new Audio('balloonPop.wav')
 //make event listener on whole gameArea
 let gameArea = document.querySelector('.game-area')
 
+//get scoreElement
 const scoreNumEl = document.querySelector('.scoreNumber')
+
+//get start button element
+const startBtnEl = document.querySelector('.start')
+
 
 //if a click hits a ball in the game-area then it
 //console.logs a message
@@ -28,16 +33,28 @@ gameArea.addEventListener('click', (e)=> {
 })
 
 //create bubbles down below with random x-coordinates, sizes, and all withinthe game-area with floatUp animations.
-setInterval(()=> {
-  let bubble = document.createElement('span')
-  bubble.classList.add('bubble')
-  let randomXPosition = (Math.floor(Math.random() * 1000)).toString()
-  let randomSize = (Math.floor(Math.random() * 300)).toString()
-  bubble.style.height = randomSize + 'px'
-  bubble.style.width = randomSize + 'px'
-  bubble.style.left = randomXPosition + 'px'
-  
-  console.log(randomXPosition + 'px');
-  gameArea.append(bubble);
-}, 1275);
+
+
+
+startBtnEl.addEventListener('click', ()=> {
+  setInterval(()=> {
+    let bubble = document.createElement('span')
+    bubble.classList.add('bubble')
+    let randomXPosition = (Math.floor(Math.random() * 1000)).toString()
+    let randomSize = (Math.floor(Math.random() * 300)).toString()
+    bubble.style.height = randomSize + 'px'
+    bubble.style.width = randomSize + 'px'
+    bubble.style.left = randomXPosition + 'px'
+    let randomColor = chooseColor()
+    bubble.style.backgroundColor = randomColor
+    console.log(randomXPosition + 'px');
+    gameArea.append(bubble);
+  }, 2275)
+})
+
+const chooseColor = ()=> {
+  let colorArray= ['rgba(238, 113, 68, .9)', 'rgb(94, 176, 91)', 'rgba(242, 163, 134, 1)', 'rgb(109, 254, 245)', 'rgb(254, 245, 109)']
+  let randomColorIndex = Math.floor(Math.random() * colorArray.length)
+  return colorArray[randomColorIndex]
+}
 
