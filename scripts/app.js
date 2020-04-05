80f6a415c05969aa0f6a6c34e7e948e636cc1765
James Sinkler <jamessinkler713@gmail.com>
2020-04-05 10:30:06 -0700

    refactors code and adds level1 pass condition

--- scripts/app.js
+++ scripts/app.js
@@ -15,6 +15,7 @@ const scoreNumEl = document.querySelector('.scoreNumber')
 const startBtnEl = document.querySelector('.start')
 
 
+
 //if a click hits a ball in the game-area then it
 //console.logs a message
 gameArea.addEventListener('click', (e)=> {
@@ -26,6 +27,7 @@ gameArea.addEventListener('click', (e)=> {
     balloonPop.play()
     //update score
     scoreNumEl.innerText = Number(scoreNumEl.innerText) + 10;
+    
     //set display to none
     //send a new bubble
   }
@@ -35,9 +37,23 @@ gameArea.addEventListener('click', (e)=> {
 //create bubbles down below with random x-coordinates, sizes, and all withinthe game-area with floatUp animations.
 
 
+const chooseColor = ()=> {
+  let colorArray= ['rgba(238, 113, 68, .9)', 'rgb(94, 176, 91)', 'rgba(242, 163, 134, 1)', 'rgb(109, 254, 245)', 'rgb(254, 245, 109)']
+  let randomColorIndex = Math.floor(Math.random() * colorArray.length)
+  return colorArray[randomColorIndex]
+}
 
 startBtnEl.addEventListener('click', ()=> {
-  setInterval(()=> {
+  generateBubbles()
+})
+
+let startPlay = () => {
+  generateBubbles()
+}
+
+let generateBubbles = ()=> {
+  let bubbleGen = setInterval(()=> {
+    checkScore(bubbleGen)
     let bubble = document.createElement('span')
     bubble.classList.add('bubble')
     let randomXPosition = (Math.floor(Math.random() * 1000)).toString()
@@ -49,12 +65,26 @@ startBtnEl.addEventListener('click', ()=> {
     bubble.style.backgroundColor = randomColor
     console.log(randomXPosition + 'px');
     gameArea.append(bubble);
-  }, 2275)
-})
-
-const chooseColor = ()=> {
-  let colorArray= ['rgba(238, 113, 68, .9)', 'rgb(94, 176, 91)', 'rgba(242, 163, 134, 1)', 'rgb(109, 254, 245)', 'rgb(254, 245, 109)']
-  let randomColorIndex = Math.floor(Math.random() * colorArray.length)
-  return colorArray[randomColorIndex]
+  }, 5000)
+}
+let stopBubbles = (interval)=> {
+  clearInterval(interval)
 }
 
+let checkScore= (interval)=> {
+  if (Number(scoreNumEl.innerText) == 50) {
+    //gameArea.style.textAlign = 'center'
+    gameArea.innerText= 'You Passed Level 1!';
+    console.log('you won')
+    stopBubbles(interval)
+  }
+}
+
+
+
+/*
+    setInterval(()=> {
+      let offset = bubble.getBoundingClientRect().top - bubble.offsetWidth;
+      console.log(offset)
+   }, 100);
+*/
