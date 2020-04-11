a448cfd7d038a0287b17150fe1ff6b2ee02d766d
James Sinkler <jamessinkler713@gmail.com>
2020-04-11 09:45:55 -0700

    refactor code for reuability between levels

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,10 @@
 'use strict'
 
-console.log('checking js file hooked up')
-
 //make an audio object
-let balloonPop = new Audio('assets/balloonPop.wav')
+const balloonPop = new Audio('assets/balloonPop.wav')
 
 //make event listener on whole gameArea
-let gameArea = document.querySelector('.game-area')
+const gameArea = document.querySelector('.game-area')
 
 //get scoreElement
 const scoreNumEl = document.querySelector('.scoreNumber')
@@ -14,31 +12,31 @@ const scoreNumEl = document.querySelector('.scoreNumber')
 //get start button element
 const startBtnEl = document.querySelector('.start')
 
+class Level {
+  constructor(level, speed, minBubSize) {
+    this.sound = balloonPop;
+    this.gameArea = gameArea;
+    this.score = scoreNumEl;
+    this.nextLevelBtn = startBtnEl;
+    this.generationSpeed = speed;
+    this.level = level;
+    this.minBubSize = minBubSize;
+  }
+  //function 
+};
 
-
-//if a click hits a ball in the game-area then it
-//console.logs a message
 gameArea.addEventListener('click', (e)=> {
   if (e.target !== gameArea) {
-    //try removeChild instead of toggle display none
-    //e.target.classList.toggle('hidden')
-    console.log('you clicked the target')
     balloonPop.play()
-    //update score
+    gameArea.removeChild(e.target)
     //add value with inverse relationship to size
     let width = Number(e.target.style.width.split('p')[0])
-    gameArea.removeChild(e.target)
     let valueToAdd = Math.floor(10000 / width)
+    //update score
     scoreNumEl.innerText = Number(scoreNumEl.innerText) + valueToAdd;
-    
-    //set display to none
-    //send a new bubble
   }
   e.stopPropagation();
 })
-
-//create bubbles down below with random x-coordinates, sizes, and all withinthe game-area with floatUp animations.
-
 
 const chooseColor = ()=> {
   let colorArray= ['rgba(238, 113, 68, .9)', 'rgb(94, 176, 91)', 'rgba(242, 163, 134, 1)', 'rgb(109, 254, 245)', 'rgb(254, 245, 109)']
@@ -46,36 +44,39 @@ const chooseColor = ()=> {
   return colorArray[randomColorIndex]
 }
 
-startBtnEl.addEventListener('click', ()=> {
-  generateBubbles()
-})
-
-let startPlay = () => {
-  generateBubbles()
-}
-
-let generateBubbles = ()=> {
-  let bubbleGen = setInterval(()=> {
-    // checkLoss(bubbleGen, 5)
-    checkScore(bubbleGen)
+const makeBubble = (minSize) => {
     let bubble = document.createElement('span')
     bubble.classList.add('bubble')
     let randomXPosition = (Math.floor(Math.random() * 1000)).toString()
-    let randomSize = (Math.floor(Math.random() * 300)).toString()
+    let randomSize = (Math.floor(Math.random() * 300) + minSize).toString()
     bubble.style.height = randomSize + 'px'
     bubble.style.width = randomSize + 'px'
     bubble.style.left = randomXPosition + 'px'
     let randomColor = chooseColor()
     bubble.style.backgroundColor = randomColor
     console.log(randomXPosition + 'px');
+    return bubble;
+}
+
+
+
+let generateBubbles = (generationSpeed)=> {
+  let bubbleGen = setInterval(()=> {
+    checkScore(bubbleGen)
+    let bubble = makeBubble(300)
     gameArea.append(bubble);
     bubble.addEventListener('animationend', ()=> {
       console.log('game-over')
       gameArea.innerText= 'You Lost. Oh no!';
       clearInterval(bubbleGen)
     });
-  }, 3000)
+  }, generationSpeed)
 }
+
+
+startBtnEl.addEventListener('click', ()=> {
+  generateBubbles(3000)
+})
 
 let checkScore= (interval)=> {
   if (Number(scoreNumEl.innerText) > 500) {
@@ -85,21 +86,3 @@ let checkScore= (interval)=> {
     stopBubbles(interval)
   }
 }
-/*
-let checkLoss = (interval, maxBubbles)=> {
-  if (gameArea.children.length > maxBubbles) {
-    console.log('you lost')
-    gameArea.innerText= 'You Lost. Oh no!';
-    stopBubbles(interval)
-  }
-}
-*/
-
-/*
-    setInterval(()=> {
-      let offset = bubble.getBoundingClientRect().top - bubble.offsetWidth;
-      console.log(offset)
-   }, 100);
-*/
-
-//write a condition that checks for 5 balls on screen, if so, game over.
